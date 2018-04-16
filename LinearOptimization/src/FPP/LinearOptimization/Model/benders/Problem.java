0aa3d98d75e9bc44faf5e625aaccffcd02d8b4c7
Martin Meier <mmeierried@gmail.com>
2018-04-16 12:40:09 +0200

    implemented first draf of benders algorithm

--- LinearOptimization/src/FPP/LinearOptimization/Model/benders/Problem.java
+++ LinearOptimization/src/FPP/LinearOptimization/Model/benders/Problem.java
@@ -16,6 +16,10 @@ abstract class Problem {
 		return function;
 	}
 	
+	public void setFunction(Double[] function) {
+		this.function = function;
+	}
+	
 	public Double[][] getRestrictions() {
 		if (restrictions.isEmpty()) {
 			return new Double[0][0];
@@ -33,33 +37,44 @@ abstract class Problem {
 	}
 	
 	public void addRestriction(Double[] restriction) {
-		if (restriction.length != getFunction().length) {
+		/*if (restriction.length != getFunction().length) {
 			// TODO: ist das so???
 			throw new IllegalStateException(
 					"Restriction has to be as long as function!");
-		}
+		}*/
 		restrictions.add(restriction);
 	}
 	
 	public Double[][] getSimplexArray() {
 		Double[] function = getFunction();
-		if (restrictions.isEmpty()) {
-			// TODO: zu klären ob das passieren darf?!
-			throw new IllegalStateException(
-					"No restrictions for problem!");
-		}
-		int size2 = restrictions.get(0).length;
-		Double[][] returnValue = new Double[restrictions.size()+1][size2];
+//		if (restrictions.isEmpty()) {
+//			// TODO: zu klären ob das passieren darf?!
+//			throw new IllegalStateException(
+//					"No restrictions for problem!");
+//		}
+		
 		int i = 0;
-		for (Double[] restriction : restrictions) {
-			for(int j = 0; j < size2; j++) {
-				returnValue[i][j] = restriction[j];
+		int size2 = function.length;
+		Double[][] returnValue = new Double[restrictions.size()+1][size2];
+		if (!restrictions.isEmpty()) {
+		
+			for (Double[] restriction : restrictions) {
+				for(int j = 0; j < size2; j++) {
+					returnValue[i][j] = restriction[j];
+				}
+				i++;
 			}
-			i++;
+			
 		}
-		for (int k = 0; k < size2; k++) {
+		for (int k = 0; k < function.length; k++) {
 			returnValue[i][k] = function[k];
 		}
+		
+//		//set value to 0, if no constant in function is existing
+//		if (returnValue[i][returnValue[i].length - 1] == null) {
+//			returnValue[i][returnValue[i].length - 1] = 0d;
+//		}
+		
 		return returnValue;
 	}
 }
