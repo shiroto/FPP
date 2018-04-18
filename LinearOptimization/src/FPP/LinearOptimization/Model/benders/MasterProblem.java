a8e4cc653ff31bc5cdca109cd5b806c376ea53db
StefanKopetzky <37866173+StefanKopetzky@users.noreply.github.com>
2018-04-18 17:42:58 +0200

    benders algorithm
    - JUnit test problem
    - add commentaries
    - beautify code

--- LinearOptimization/src/FPP/LinearOptimization/Model/benders/MasterProblem.java
+++ LinearOptimization/src/FPP/LinearOptimization/Model/benders/MasterProblem.java
@@ -2,14 +2,26 @@ package FPP.LinearOptimization.Model.benders;
 
 public class MasterProblem extends Problem {
 
+	// internal variables
 	private BendersMasterCoefficientType[] types;
 	
+	/**
+	 * Initializes a master problem by a given simplex tableau.<br><br>
+	 * 
+	 * Tableau needs to be given in this way:<br>
+	 * {{1d, 2d, 0d, 0d, 200d}, <b>restriction 1</b><br>
+	 * {1d, 0d, 0d, 0d, 50d}, <b>restriction 2</b><br>
+	 * {0d, 0d, 1d, 0d, 80d}, <b>restriction 3</b><br>
+	 * {0d, 0d, 3d, 10d, 500d}} <b>function</b>
+	 * 
+	 * @param simplexTableau
+	 */
 	public MasterProblem(Double[][] simplexTableau) {
 		super(simplexTableau);
 	}
 	
 	@Override
-	public boolean isSolveableWithBAndB() {
+	public boolean isSolvableWithBAndB() {
 		for (Double f : getFunction()) {
 			// we check if we have floats or ints 
 			int sqrt = (int) Math.round(Math.sqrt(f));
@@ -18,14 +30,30 @@ public class MasterProblem extends Problem {
 				return false;
 			}
 		}
-		// only ints, so branch and bound is allowed here 
+		// only integers, so branch and bound is allowed here 
 		return true;
 	}
 	
+	/**
+	 * Defines the type of the master problem function coefficients.<br><br>
+	 * 
+	 * <b>example function:</b><br>
+	 * {0d, 0d, 3d, 10d, 500d}<br><br>
+	 * 
+	 * <b>example types:</b><br>
+	 * {I, I, I, F}
+	 * 
+	 * @return coefficient type
+	 */
 	public BendersMasterCoefficientType[] getTypes() {
 		return types;
 	}
 	
+	/**
+	 * Set the coefficient type for the master problem function.
+	 * 
+	 * @param types
+	 */
 	public void setTypes(BendersMasterCoefficientType[] types) {
 		this.types = types;
 	}
--- LinearOptimization/src/FPP/LinearOptimization/Model/benders/Problem.java
+++ LinearOptimization/src/FPP/LinearOptimization/Model/benders/Problem.java
@@ -3,18 +3,38 @@ package FPP.LinearOptimization.Model.benders;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This represents the problem in form of a simplex tableau.<br>
+ * Functions, coefficients and the objective function values are saved separately,<br>
+ * so you always can generate a simplex tableau of the current problem.
+ * 
+ * @author Stefan
+ */
 public class Problem {
 
+	// internal variables
 	private Double[] function = new Double[0];
 	private List<Double[]> coefficients = new ArrayList<>();
 	private List<Double> b = new ArrayList<Double>();
 	
+	/**
+	 * Initializes a problem by a given simplex tableau.<br><br>
+	 * 
+	 * Tableau needs to be given in this way:<br>
+	 * {{1d, 2d, 0d, 0d, 200d}, <b>restriction 1</b><br>
+	 * {1d, 0d, 0d, 0d, 50d}, <b>restriction 2</b><br>
+	 * {0d, 0d, 1d, 0d, 80d}, <b>restriction 3</b><br>
+	 * {0d, 0d, 3d, 10d, 500d}} <b>function</b>
+	 * 
+	 * @param simplexTableau
+	 */
 	public Problem(Double[][] simplexTableau) {
+		// define dimension of function and coefficients
 		int restrictionCNT = simplexTableau.length-1;
 		int coefficientLength = simplexTableau[0].length-1;
 		int functionLength = simplexTableau[0].length;
 		
-		// extract function of simplex tableau
+		// extract function of simplex tableau (last line)
 		Double[] function = new Double[functionLength];
 		for (int i = 0; i < functionLength; i++) {
 			function[i] = simplexTableau[restrictionCNT][i];
@@ -25,6 +45,7 @@ public class Problem {
 			/*
 			 * Restrictions are given, so add them to the problem,
 			 * and divide them into coefficients and objective function values (b)
+			 * (IGNORE last line, because thats already our function)
 			 */
 			for (int i = 0; i < restrictionCNT; i++) {
 				Double[] c = new Double[coefficientLength];
@@ -37,14 +58,46 @@ public class Problem {
 		}
 	}
 	
+	/**
+	 * Returns the function of the current problem.<br><br>
+	 * 
+	 * <b>example tableau:</b><br>
+	 * {{1d, 2d, 0d, 0d, 200d}, <b>restriction 1</b><br>
+	 * {1d, 0d, 0d, 0d, 50d}, <b>restriction 2</b><br>
+	 * {0d, 0d, 1d, 0d, 80d}, <b>restriction 3</b><br>
+	 * {0d, 0d, 3d, 10d, 500d}} <b>function</b><br><br>
+	 * 
+	 * In this case only the function line is returned:<br>
+	 * <b>{0d, 0d, 3d, 10d, 500d}</b>
+	 * 
+	 * @return function
+	 */
 	public Double[] getFunction() {
 		return function;
 	}
 	
+	/**
+	 * Updates the function of the problem.
+	 * 
+	 * @param function
+	 */
 	public void setFunction(Double[] function) {
 		this.function = function;
 	}
 	
+	/**
+	 * Returns the object function values of the method restrictions.<br><br>
+	 * 
+	 * <b>example restrictions:</b><br>
+	 * {{1d, 2d, 0d, <b>0d</b>},<br>
+	 * {1d, 0d, 0d, <b>0d</b>},<br>
+	 * {0d, 0d, 1d, <b>0d</b>}}<br><br>
+	 * 
+	 * Marked values of restrictions are returned this way:<br>
+	 * <b>{0d, 0d, 0d}</b>
+	 * 
+	 * @return object function values
+	 */
 	public Double[] getB() {
 		Double[] returnValue = new Double[b.size()];
 		for (int i = 0; i < b.size(); i++) {
@@ -53,47 +106,104 @@ public class Problem {
 		return returnValue;
 	}
 	
+	/**
+	 * This returns all coefficients of the problem restrictions.<br><br>
+	 * 
+	 * <b>example coefficients:</b><br>
+	 * {{1d, 2d, 0d},<br>
+	 * {1d, 0d, 0d},<br>
+	 * {0d, 0d, 1d}}<br><br>
+	 * 
+	 * See difference to {@link #getRestrictions()}!
+	 * 
+	 * @return coefficients array
+	 */
 	public Double[][] getCoefficients() {
+		// check if there are any coefficients
 		if (coefficients.isEmpty()) {
 			return new Double[0][0];
 		}
-		int coefficientLength = coefficients.get(0).length;
-		Double[][] returnValue = new Double[coefficients.size()][coefficientLength];
-		int i = 0;
-		for (Double[] coefficient : coefficients) {
-			for(int j = 0; j < coefficientLength; j++) {
-				returnValue[i][j] = coefficient[j];
-			}
-			i++;
-		}
-		return returnValue;
-	}
-	
-	public Double[][] getRestrictions() {
-		if (coefficients.isEmpty()) {
-			return new Double[0][0];
-		}
-		// objective function values have to be added to the coefficients too
-		int restrictionLength = coefficients.get(0).length+1;
 		
-		// define size of return value
-		Double[][] returnValue = new Double[coefficients.size()][restrictionLength];
-		int i = 0;
-		for (Double[] restriction : coefficients) {
-			for(int j = 0; j < restrictionLength-1; j++) {
-				returnValue[i][j] = restriction[j];
+		// define size of coefficients array
+		int coefficientCnt = coefficients.size();
+		int coefficientLength = coefficients.get(0).length;
+		
+		// initialize new array for coefficients
+		Double[][] coefficientsArray = new Double[coefficientCnt][coefficientLength];
+		
+		// fill coefficients array
+		for (int i = 0; i < coefficientCnt; i++) {
+			for(int j = 0; j < coefficientLength; j++) {
+				coefficientsArray[i][j] = coefficients.get(i)[j];
 			}
-			returnValue[i][restrictionLength-1] = b.get(i);
-			i++;
 		}
-		return returnValue;
+		return coefficientsArray;
 	}
 	
+	/**
+	 * This returns all restrictions of the problem.<br><br>
+	 * 
+	 * <b>example restrictions:</b><br>
+	 * {{1d, 2d, 0d, 0d},<br>
+	 * {1d, 0d, 0d, 0d},<br>
+	 * {0d, 0d, 1d, 0d}}<br><br>
+	 * 
+	 * See difference to {@link #getCoefficients()}!
+	 * 
+	 * @return restrictions
+	 */
+	public Double[][] getRestrictions() {
+		// check if there are any coefficients
+		if (coefficients.isEmpty()) {
+			return new Double[0][0];
+		}
+		// define size of restriction array
+		int restrictionLength = coefficients.get(0).length+1;
+		int restrictionCnt = coefficients.size();
+		
+		// initialize new restrictions array
+		Double[][] restrictionsArray = new Double[restrictionCnt][restrictionLength];
+		
+		// fill array with coefficients and objective function values
+		for (int i = 0; i < restrictionCnt; i++) {
+			for(int j = 0; j < restrictionLength-1; j++) {
+				restrictionsArray[i][j] = coefficients.get(i)[j];
+			}
+			// objective function values have to be added to the coefficients too
+			restrictionsArray[i][restrictionLength-1] = b.get(i);
+		}
+		return restrictionsArray;
+	}
+	
+	/**
+	 * This adds a restriction to the problem.<br><br>
+	 * 
+	 * <b>example restriction:</b><br>
+	 * {1d, 2d, 0d, 0d}<br>
+     * <p><ul>
+     * <li>1d, 2d, 0d => <b>coefficients</b>
+     * <li>0d => <b>b</b>
+     * </ul><p>
+	 * 
+	 * @param coefficients
+	 * @param b objective function value
+	 */
 	public void addRestriction(Double[] coefficients, Double b) {
 		this.coefficients.add(coefficients);
 		this.b.add(b);
 	}
 	
+	/**
+	 * Returns a simplex tableau of the current problem.<br><br>
+	 * 
+	 * <b>example tableau:</b><br>
+	 * {{1d, 2d, 0d, 0d, 200d}, <b>restriction 1</b><br>
+	 * {1d, 0d, 0d, 0d, 50d}, <b>restriction 2</b><br>
+	 * {0d, 0d, 1d, 0d, 80d}, <b>restriction 3</b><br>
+	 * {0d, 0d, 3d, 10d, 500d}} <b>function</b><br>
+	 * 
+	 * @return simplex tableau
+	 */
 	public Double[][] getSimplexTableau() {
 		// define size of tableau
 		int tableauLength = coefficients.size() + 1;
@@ -112,7 +222,12 @@ public class Problem {
 		return simplexTableau;
 	}
 	
-	public boolean isSolveableWithBAndB() {
+	/**
+	 * Defines if this problem is solvable with <b>branch and bound</b> algorithm.<br>
+	 * 
+	 * @return boolean
+	 */
+	public boolean isSolvableWithBAndB() {
 		return false;
 	}
 }
