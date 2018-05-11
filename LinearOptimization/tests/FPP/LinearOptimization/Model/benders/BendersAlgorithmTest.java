f6fa5e3f1e1756fa2ea94ebcd28ebd0e845f7308
StefanKopetzky <37866173+StefanKopetzky@users.noreply.github.com>
2018-05-11 15:52:07 +0200

    benders algorithm test
    - add some examples

--- LinearOptimization/tests/FPP/LinearOptimization/Model/benders/BendersAlgorithmTest.java
+++ LinearOptimization/tests/FPP/LinearOptimization/Model/benders/BendersAlgorithmTest.java
@@ -198,6 +198,210 @@ public class BendersAlgorithmTest {
 		testInput(inputData, result, parameterResults);
 	}
 	
+	/*
+	 * Branch and Bound Minimierung
+	 * Keine Deklaration von Y Variablen!
+	 *
+	 * Double [][] array= {
+				{-4d,-4d,-10d},
+				{-2d,-11d,-11d},
+				{-4d,2d,1d},
+				{2d,1d,0d}};
+	 * --> 1,2, Zielfunktionswert=4
+	 *
+	 * https://www.ingenieurkurse.de/operations-research-2/ganzzahlige-optimierung/branch-and-bound-verfahren/minimierungsprobleme/branch-and-bound-am-minimierungsproblem-optimale-loesung/beispiel-branch-and-bound-am-minimierungsproblem-optimale-loesung-3.html
+	 */
+//	@Test
+	void test_example_06() {
+		System.out.println("test_example_06");
+		// prepare test data
+		
+		Double[] function = {-8d, -6d, 2d, 42d, 18d, 33d, 0d};
+		Double[][] simplexTableau = new Double[][] {
+			{-4d,-4d,-10d},
+			{-2d,-11d,-11d},
+			{-4d,2d,1d},
+			{2d,1d,0d}, 
+			function};
+		int[] paramaterNegativeIndices = {};
+		int[] yVariableIndices = {};
+		BendersMasterCoefficientType[] yTypes = {};
+		
+		BendersOptimizationData inputData = new BendersOptimizationData(simplexTableau, 
+				paramaterNegativeIndices, yVariableIndices, yTypes);
+		
+		double result = 4d;
+		double[] parameterResults = {1d, 2d};
+		
+		testInput(inputData, result, parameterResults);
+	}
+	
+	/*
+	 * 0,1 Problem
+	 * 		Double [][] array= {    
+				{8d,8d,6d,6d,14d},
+				{1d,0d,0d,0d,1d},
+				{-1d,0d,0d,0d,0d},
+				{0d,1d,0d,0d,1d},
+				{0d,-1d,0d,0d,0d},
+				{0d,0d,1d,0d,1d},
+				{0d,0d,-1d,0d,0d},
+				{0d,0d,0d,1d,1d},
+				{0d,0d,0d,-1d,0d},
+				{-14d,-20d,-18d, -26d,0d}};
+	 * --> [0.0, 1.0, 0.0, 1.0, 46.0]
+	 * 
+	 * https://www.ingenieurkurse.de/operations-research-2/ganzzahlige-optimierung/branch-and-bound-verfahren/branch-and-bound-knapsack-problem.html  
+	 */
+//	@Test
+	void test_example_07() {
+		System.out.println("test_example_07");
+		// prepare test data
+		Double[] function = {-14d,-20d,-18d, -26d, 0d};
+		Double[][] simplexTableau = new Double[][] {
+			{8d,8d,6d,6d,14d},
+			{1d,0d,0d,0d,1d},
+			{-1d,0d,0d,0d,0d},
+			{0d,1d,0d,0d,1d},
+			{0d,-1d,0d,0d,0d},
+			{0d,0d,1d,0d,1d},
+			{0d,0d,-1d,0d,0d},
+			{0d,0d,0d,1d,1d},
+			{0d,0d,0d,-1d,0d},
+			function};
+		int[] paramaterNegativeIndices = {};
+		int[] yVariableIndices = {};
+		BendersMasterCoefficientType[] yTypes = {};
+		
+		BendersOptimizationData inputData = new BendersOptimizationData(simplexTableau, 
+				paramaterNegativeIndices, yVariableIndices, yTypes);
+		
+		double result = 46d;
+		double[] parameterResults = {0d, 1d, 0d, 1d};
+		
+		testInput(inputData, result, parameterResults);
+	}
+	
+	/*
+	 * 0 1 Problem
+	 * 
+	 * 	Double [][] array= {
+				{30d,50d,20d,70d},
+				{1d,0d,0d,1d},
+				{-1d,0d,0d,0d},
+				{0d,1d,0d,1d},
+				{0d,-1d,0d,0d},
+				{0d,0d,1d,1d},
+				{0d,0d,-1d,0d},
+				{-3d,-5d,-2d,0d}};
+	 * --> [0.0, 1.0, 1.0, 7.0]		
+	 */
+//	@Test
+	void test_example_08() {
+		System.out.println("test_example_08");
+		// prepare test data
+		Double[] function = {-3d,-5d,-2d,0d};
+		Double[][] simplexTableau = new Double[][] {
+			{30d,50d,20d,70d},
+			{1d,0d,0d,1d},
+			{-1d,0d,0d,0d},
+			{0d,1d,0d,1d},
+			{0d,-1d,0d,0d},
+			{0d,0d,1d,1d},
+			{0d,0d,-1d,0d},
+			function};
+		int[] paramaterNegativeIndices = {};
+		int[] yVariableIndices = {2};
+		BendersMasterCoefficientType[] yTypes = {BendersMasterCoefficientType.Float};
+		BendersOptimizationData inputData = new BendersOptimizationData(simplexTableau, 
+				paramaterNegativeIndices, yVariableIndices, yTypes);
+		
+		double result = 7d;
+		double[] parameterResults = {0d, 1d, 1d};
+		
+		testInput(inputData, result, parameterResults);
+	}
+	
+	/*
+	 * BINÄR
+		Double [][] array= {
+				{6d,3d,5d,2d,10d},
+				{0d,0d,1d,1d,1d},
+				{-1d,0d,1d,0d,0d},
+				{0d,-1d,0d,1d,0d},
+				{1d,0d,0d,0d,1d},		
+				{-1d,0d,0d,0d,0d},
+				{0d,1d,0d,0d,1d},
+				{0d,-1d,0d,0d,0d},
+				{0d,0d,1d,0d,1d},
+				{0d,0d,-1d,0d,0d},
+				{0d,0d,0d,1d,1d},
+				{0d,0d,0d,-1d,0d},
+				
+				{-9d,-5d,6d,4d,0d}};
+	 * --> [1.0, 1.0, 0.0, 0.0, 14.0]
+	 * https://members.loria.fr/CRingeissen/files/master2/solving-lp-ip.pdf
+	 */
+//	@Test
+	void test_example_09() {
+		System.out.println("test_example_09");
+		// prepare test data
+		Double[] function = {-9d,-5d,6d,4d,0d};
+		Double[][] simplexTableau = new Double[][] {
+			{6d,3d,5d,2d,10d},
+			{0d,0d,1d,1d,1d},
+			{-1d,0d,1d,0d,0d},
+			{0d,-1d,0d,1d,0d},
+			{1d,0d,0d,0d,1d},		
+			{-1d,0d,0d,0d,0d},
+			{0d,1d,0d,0d,1d},
+			{0d,-1d,0d,0d,0d},
+			{0d,0d,1d,0d,1d},
+			{0d,0d,-1d,0d,0d},
+			{0d,0d,0d,1d,1d},
+			{0d,0d,0d,-1d,0d},
+			function};
+		int[] paramaterNegativeIndices = {};
+		int[] yVariableIndices = {};
+		BendersMasterCoefficientType[] yTypes = {};
+		
+		BendersOptimizationData inputData = new BendersOptimizationData(simplexTableau, 
+				paramaterNegativeIndices, yVariableIndices, yTypes);
+		
+		double result = 14d;
+		double[] parameterResults = {1d, 1d, 0d, 0d};
+		
+		testInput(inputData, result, parameterResults);
+	}
+	/*
+	 * https://members.loria.fr/CRingeissen/files/master2/solving-lp-ip.pdf
+	 */
+	@Test
+	void test_example_10() {
+		System.out.println("test_example_10");
+		// prepare test data
+		Double[] function = {11d,14d,0d,0d,0d, 0d, 0d};
+		Double[][] simplexTableau = new Double[][] {
+			{1d, 1d, 1d, 0d, 0d, 0d, 17d},
+			{3d, 7d, 0d, 1d, 0d, 0d, 63d},
+			{3d, 5d, 0d, 0d, 1d, 0d, 48d},
+			{3d, 1d, 0d, 0d, 0d, 1d, 30d},
+			function};
+			int[] paramaterNegativeIndices = {};
+			int[] yVariableIndices = {2, 3, 4, 5};
+			BendersMasterCoefficientType[] yTypes = {BendersMasterCoefficientType.Binary,
+					BendersMasterCoefficientType.Binary,BendersMasterCoefficientType.Binary,
+					BendersMasterCoefficientType.Binary};
+			
+			BendersOptimizationData inputData = new BendersOptimizationData(simplexTableau, 
+					paramaterNegativeIndices, yVariableIndices, yTypes);
+			
+			double result = 0d;
+			double[] parameterResults = {0d, 0d, 0d, 0d, 0d, 0d};
+			
+			testInput(inputData, result, parameterResults);
+	}
+	
 	void testInput(BendersOptimizationData input, double expectedResult, double[] expectedParameterResults) {
 		IBendersOptimizationSolutionData solutionData = new BendersAlgorithm().solve(input);
 		Double[] optSolution = solutionData.getOptSolution();
